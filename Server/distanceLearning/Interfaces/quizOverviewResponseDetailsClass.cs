1fd6c60d8fcc11920d24c305d58813209121e104
vsalmiery1 <M!pa55w0rd>
2017-06-08 15:19:15 -0400

    working

---
+++ Server/distanceLearning/Interfaces/quizOverviewResponseDetailsClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contensive.BaseClasses;
+using Contensive.Addons.DistanceLearning.Models;
+using Contensive.Addons.DistanceLearning.Interfaces;
+using Contensive.Addons.DistanceLearning.Controllers;
+
+namespace Contensive.Addons.DistanceLearning
+{
+    class quizOverviewResponseDetailsClass : Contensive.BaseClasses.AddonBaseClass
+
+        {
+            public override object Execute(CPBaseClass cp)
+            {
+                string result = "Hello World";
+                try
+                {
+                    
+                   
+                }
+                catch (Exception ex)
+                {
+                    errorReport(cp, ex, "execute");
+                }
+                return result;
+
+
+            }
+            //
+            // ===============================================================================
+            // handle errors for this class
+            // ===============================================================================
+            //
+            private void errorReport(CPBaseClass cp, Exception ex, string method)
+            {
+                cp.Site.ErrorReport(ex, "error in addonTemplateCs2005.blankClass.getForm");
+            }
+        }
+    }
+
+
