db21fea269df8ee64e61923b4f54e6a1fb7ad984
vsalmiery1 <M!pa55w0rd>
2017-06-08 12:30:42 -0400

    Update Dashboard

--- Server/distanceLearning/Interfaces/quizOverviewDetailsClass.cs
+++ Server/distanceLearning/Interfaces/quizOverviewDetailsClass.cs
@@ -71,15 +71,21 @@ namespace Contensive.Addons.DistanceLearning
                 qs = cp.Doc.RefreshQueryString;
                 qs = cp.Utils.ModifyQueryString(qs, "dstFeatureGuid", constants.portalFeatureQuizOverviewSetting, true);
                 qs = cp.Utils.ModifyQueryString(qs, "QuizId", quiz.id.ToString(), true);
-                form.rowValue = ("<div><img src=/images/NavRecord.gif display:inline;><a href=\"?" + qs + "\">" + quiz.questionPresentation + " ; Using Subjects; " + quiz.includeSubject + "User can retake quiz:  " + quiz.allowRetake + "; Max " + quiz.maxNumberQuest + " Questions </a></div>");
+                form.rowValue = ("<div><a href=\"?" + qs + "\"><img src=\"/myDistanceLearning/NavRecord.gif\" display:inline;></a>" + quiz.questionPresentation + " ; Using Subjects; " + quiz.includeSubject + "User can retake quiz:  " + quiz.allowRetake + "; Max " + quiz.maxNumberQuest + " Questions </div>");
                 form.addRow();
-                form.rowValue = ("<div><img src=/images/NavRecord.gif display:inline;><a href=\"?" + qs + "\">" + quiz.maxNumberQuest + " total Questions in Quiz</a></div>");
+                form.rowValue = ("<div><a href=\"?" + qs + "\"><img src=\"/myDistanceLearning/NavRecord.gif\" display:inline;></a>" + quiz.maxNumberQuest + " total Questions in Quiz</div>");
                 qs = cp.Doc.RefreshQueryString;
-                qs = cp.Utils.ModifyQueryString(qs, "dstFeatureGuid", constants.portalFeatureQuizOverviewScoring);
-                    form.addRow();
-                form.rowValue = ("<div><img src=/images/NavRecord.gif display:inline;><a href=\"?" + qs + "\"> Mastery " + quiz.APercentile + "+ </b>      Above Average " + quiz.BPercentile + "+ </br></a></div>");
-                // -- wrap in tabs and output finished form
-                result = genericController.getTabWrapper(cp, form.getHtml(cp), "Details",quiz);
+                qs = cp.Utils.ModifyQueryString(qs, "dstFeatureGuid", constants.portalFeatureQuizOverviewScoring,true);
+                qs = cp.Utils.ModifyQueryString(qs, "QuizId", quiz.id.ToString(), true);
+                form.addRow();
+
+                form.rowValue = ("<div><a href=\"?" + qs + "\"><img src=\"/myDistanceLearning/NavRecord.gif\" display:inline;></a> Mastery " + quiz.APercentile + "+ </br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Above Average " 
+                    + quiz.BPercentile + "+ </br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Acceptable "
+                    + quiz.CPercentile + "+ </br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Needs More Work "
+                    + quiz.DPercentile + "+ </br> &nbsp;&nbsp;&nbsp;&nbsp; Successful Completions adds "
+                    + quiz.certificationCECs + " CECs to user accredidation </br></div>");
+                 // -- wrap in tabs and output finished form
+                 result = genericController.getTabWrapper(cp, form.getHtml(cp), "Details",quiz);
                 cp.Doc.AddHeadStyle(form.styleSheet);
             }
             catch (Exception ex)
