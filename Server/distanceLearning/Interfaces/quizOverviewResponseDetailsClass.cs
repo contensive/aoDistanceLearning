211303df73deb46baa99d7f97d3546ae22d2fdaf
jay <jay@contensive.com>
2017-06-09 17:14:27 -0400

    result detail tab

--- Server/distanceLearning/Interfaces/quizOverviewResponseDetailsClass.cs
+++ Server/distanceLearning/Interfaces/quizOverviewResponseDetailsClass.cs
@@ -9,16 +9,36 @@ using Contensive.Addons.DistanceLearning.Controllers;
 namespace Contensive.Addons.DistanceLearning
 {
     public class quizOverviewResponseDetailsClass : Contensive.BaseClasses.AddonBaseClass
-
+    {
+        public override object Execute(CPBaseClass cp)
         {
-            public override object Execute(CPBaseClass cp)
+            string result = "";
+            try
             {
-                string result = "";
-                try
+                int responseId = cp.Doc.GetInteger("responseid");
+                Models.QuizResponseModel response = Models.QuizResponseModel.create(cp, responseId);
+                Models.QuizModel quiz = Models.QuizModel.create(cp, response.QuizID);
+                if (cp.Doc.GetText(constants.rnButton) == constants.buttonCancel)
                 {
-                cp.Doc.SetProperty("id", cp.Doc.GetInteger("responseid").ToString());
-                result = cp.Utils.ExecuteAddon(constants.scoreCardAddon);
+                    // -- go back to response
+                    string qs = cp.Doc.RefreshQueryString;
+                    qs = cp.Utils.ModifyQueryString(qs, "dstFeatureGuid", constants.portalFeatureQuizOverviewResults, true);
+                    qs = cp.Utils.ModifyQueryString(qs,  constants.rnQuizId, quiz.id.ToString() , true);
+                    cp.Response.Redirect("?" + qs);
+                    return "";
                 }
+                adminFramework.formSimpleClass form = new adminFramework.formSimpleClass();
+                form.title = "Quiz Response";
+                form.description = ""
+                    + cp.Html.div("Date Completed: " + response.dateSubmitted.ToShortDateString() )
+                    + cp.Html.div("Participant: #" + response.MemberID)
+                    + "";
+                cp.Doc.SetProperty("id", responseId.ToString());
+                form.body = cp.Utils.ExecuteAddon(constants.scoreCardAddon);
+                form.addFormButton(constants.buttonCancel);
+                result = form.getHtml(cp);
+                result = genericController.getTabWrapper(cp, result, "Results", quiz);
+            }
             catch (Exception ex)
                 {
                     errorReport(cp, ex, "execute");
